bc8b24e9213931ce57381dad720acf6530b18fc9
ssrkarthikeya <Hachinov1>
2021-11-30 11:37:37 +0530

    initial

---
+++ src/main/java/io/icegadda/goodwatchratingsservice/GoodwatchRatingsServiceApplication.java
@@ -0,0 +1,13 @@
+package io.icegadda.goodwatchratingsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodwatchRatingsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodwatchRatingsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/icegadda/goodwatchratingsservice/GoodwatchRatingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.icegadda.goodwatchratingsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodwatchRatingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
